76134843b6d00c898cbf4b7eddaa9f1dbb234a4d
Alvian Zachry Faturrahman <alvianzf@macbooks-MacBook-Air.local>
2019-04-15 23:51:32 +0700

    design page part 1

---
+++ src/Container.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="container">
+        <h3>{{message}}</h3>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'container',
+    data () {
+        return {
+            message: 'This sould be the main page'
+        }
+    }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import BootstrapVue from "bootstrap-vue"
 import App from './App.vue'
+import Container from './Container.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
@@ -8,5 +9,5 @@ Vue.use(BootstrapVue)
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(Container)
 })
