b63536a6b6b36ee5774dcb91947d127e1867b235
Alvian Zachry Faturrahman <alvianzf@macbooks-MacBook-Air.local>
2019-05-06 20:43:56 +0700

    vuex and vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,29 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+    <v-toolbar app dark color="info">
+      <v-toolbar-title class="headline">
+        <span>Find</span>
+        <span class="font-weight-light">Hotel</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
         flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
+        to='/'
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">Dashboard</span>
       </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   },
   data () {
     return {
